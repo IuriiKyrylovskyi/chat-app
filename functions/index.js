583598d649a65f7902f4b2974f714f531f053c0e
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-04 00:42:30 +0300

    funcs

--- functions/index.js
+++ functions/index.js
@@ -14,27 +14,19 @@ exports.detectEvilUsers = functions.firestore
 
     if (filter.isProfane(text)) {
       const cleaned = filter.clean(text);
-
-      await doc.ref.update({ text: `🤐 I got BANNED for life for saing.` });
-
+      await doc.ref.update({ text: `🤐 I got BANNED for life for saying... ${cleaned}` });
+      
       await db.collection('banned').doc(uid).set({});
     }
 
-     const userRef = db.collection('users').doc(uid);
+    const userRef = db.collection('users').doc(uid);
 
-     const userData = (await userRef.get()).data();
+    const userData = (await userRef.get()).data();
 
-     if (userData.msgCount >= 7) {
-       await db.collection('banned').doc(uid).set({});
-     } else {
-       await userRef.set({ msgCount: (userData.msgCount || 0) + 1 });
-     }
+    if (userData.msgCount >= 7) {
+      await db.collection('banned').doc(uid).set({});
+    } else {
+      await userRef.set({ msgCount: (userData.msgCount || 0) + 1 });
+    }
   });
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
